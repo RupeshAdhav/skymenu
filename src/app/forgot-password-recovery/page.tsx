9d347b25c8d75bffad386f0b6866c5a538e19dd1
RupeshAdhav <adhavrupesh84@gmail.com>
2024-06-27 09:24:16 +0530

    added suspense in forgot password recovery page

--- src/app/forgot-password-recovery/page.tsx
+++ src/app/forgot-password-recovery/page.tsx
@@ -17,11 +17,12 @@ export default function ForgotPasswordConfirmation() {
     const [disabledButton, setDisabledButton] = useState(false);
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
+    const [email, setEmail] = useState('');
 
-    // const router = useRouter();
-    // const searchParams = useSearchParams();
-    // const userId = String(searchParams.get('userId'));
-    // const secret = String(searchParams.get('secret'));
+    const router = useRouter();
+    const searchParams = useSearchParams();
+    const userId = String(searchParams.get('userId'));
+    const secret = String(searchParams.get('secret'));
 
     const handleSubmit = async (event: any) => {
         event.preventDefault();
@@ -32,15 +33,15 @@ export default function ForgotPasswordConfirmation() {
 
                     setDisabledButton(true);
 
-                    // updateForgotPassword(userId, secret, password, confirmPassword)
-                    // .then(() => {
-                    //     setNewPassword(true);
-                    //     setDisabledButton(false);
-                    // })
-                    // .catch((err: any) => {
-                    //     toast.error(err.message); 
-                    //     setDisabledButton(false);
-                    // });
+                    updateForgotPassword(userId, secret, password, confirmPassword)
+                    .then(() => {
+                        setNewPassword(true);
+                        setDisabledButton(false);
+                    })
+                    .catch((err: any) => {
+                        toast.error(err.message); 
+                        setDisabledButton(false);
+                    });
 
                 } catch (err: any) {
                     toast.error(JSON.stringify(err)); 
@@ -55,12 +56,12 @@ export default function ForgotPasswordConfirmation() {
     }
 
     useEffect(() => {
-        // if(searchParams.has('userId') || searchParams.has('secret')){
-        //     setDataLoaded(true);
-        // }else{
-        //     toast.error('User id not found');
-        //     router.push('/not-found');
-        // }
+        if(searchParams.has('userId') || searchParams.has('secret')){
+            setDataLoaded(true);
+        }else{
+            toast.error('User id not found');
+            router.push('/not-found');
+        }
      // eslint-disable-next-line react-hooks/exhaustive-deps
      }, []);
 
@@ -85,7 +86,7 @@ export default function ForgotPasswordConfirmation() {
                                     </div>
                                 : 
                                     <form className="space-y-6" onSubmit={handleSubmit} autoComplete="off">
-                                        {/* <div>
+                                        <div>
                                             <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                                                 Email address
                                             </label>
@@ -99,7 +100,7 @@ export default function ForgotPasswordConfirmation() {
                                                 className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                                                 />
                                             </div>
-                                        </div> */}
+                                        </div>
 
                                         <div>
                                             <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
@@ -146,6 +147,6 @@ export default function ForgotPasswordConfirmation() {
                         </div>
                     </div>
             }
-       </Suspense>
+        </Suspense>
     )
 }
